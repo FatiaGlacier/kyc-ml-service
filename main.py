850f9fe36c659af894a2fbb56740d92ca10c37cb
FatiaGlacier <dmfatianov@gmail.com>
2026-01-03 22:57:06 +0200

    video endpoint

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Servus!"}
+
+@app.post("/send-video/")
+async def process_video(video: UploadFile = File(...)):
+    return {"message": "Servus!", "video_name" : video.filename}
